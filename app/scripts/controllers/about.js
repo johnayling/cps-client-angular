31d2558f811caa7ad55b6638f960159019de440c
AYLING <jxayl0@corp.qed.qld.gov.au>
2016-10-11 11:41:46 +1000

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cpsClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cpsClientApp
+ */
+angular.module('cpsClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cpsClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cpsClientApp
+ */
+angular.module('cpsClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
